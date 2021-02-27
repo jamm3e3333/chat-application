ffdae20d8bc78c720999e0f27439c5b81022b018
Jakub Vala <jamm3e3333@gmail.com>
2021-02-27 13:18:13 +0100

    client-server relationship with socket.io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,51 @@
-io();
+const socket = io();
+
+socket.on('message',(message)=>{
+    console.log(message);
+});
+
+socket.on('shareLocation', (location) => {
+    console.log(location);
+})
+
+socket.on('rating', (rate) => {
+    console.log(rate);
+})
+
+document.querySelector('#form1').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = e.target.elements.messageId.value;
+
+    socket.emit('sendMessage', message, (error) => {
+        if(error){
+            return alert(`The sentence "${error}" contains bad words.`);
+        }
+        console.log('Message delievered.');
+    });
+});
+
+document.querySelector('#form2').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const rating = e.target.elements.messageIdRate.value;
+    socket.emit('sendRating',rating, (ack) => {
+        if(ack){
+            return alert(`${ack} is not a number or is not in a range from 0 to 5.`);
+        }
+        console.log('Rating delievered.');
+    })
+})
+
+document.querySelector('#btn-loc').addEventListener('click',(e) =>{
+    if(!navigator.geolocation){
+        return alert('Your browser doen\'t support geolocation.');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        const {coords} = position;
+        socket.emit('shareLocation',{
+                latitude: coords.latitude,
+                longitude: coords.longitude
+        }, () => {
+            console.log('Location shared.');
+        });
+    })
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
+const filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,9 +14,39 @@ const publicPath = path.join(__dirname, '../public/');
 app.use(express.json());
 app.use(express.static(publicPath));
 
-
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New web socket connection.');
+
+    socket.emit('message','Welcome.');
+    socket.broadcast.emit('message','A new user has joined.');
+
+    socket.on('sendMessage', (msg, cb) => {
+        const filt = new filter();
+
+        if(filt.isProfane(msg)){
+            return cb(msg);
+        }
+
+        io.emit('message', msg);
+        cb();
+    });
+
+    socket.on('shareLocation',(position,cb) => {
+        io.emit('shareLocation', `https://google.com/map?q=${position.latitude},${position.longitude}`);
+        cb();
+    })
+
+    socket.on('sendRating',(rate,cb) => {
+        if(isNaN(parseInt(rate)) || parseInt(rate) < 0 || parseInt(rate) > 5){
+            return cb(rate);
+        }
+        io.emit('rating',rate);
+        cb();
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message','A user has left.');
+    })
 })
 server.listen(port, () => {
     console.log(`Listening on port ${port}`);
